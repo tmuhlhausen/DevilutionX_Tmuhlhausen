eae2ba64586a325b6c87993f0f43143b5c2884fb
tmuhlhausen <58753672+tmuhlhausen@users.noreply.github.com>
2026-04-25 21:38:27 -0400

    Make raid mod API header-backed

--- Source/raid/raid_mod_api.hpp
+++ Source/raid/raid_mod_api.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <cstdint>
@@ -26,13 +27,69 @@ struct RaidModHooksV1 {
 	void (*onRaidFailed)(const RaidInstanceState &state) = nullptr;
 };
 
-void ResetRaidModHooks();
-[[nodiscard]] bool RegisterRaidModHooks(std::string_view moduleName, uint32_t apiVersion, const RaidModHooksV1 &hooks);
-[[nodiscard]] RaidModCompatibilityStatus GetRaidModCompatibilityStatus();
+namespace raid_mod_api_detail {
 
-void NotifyRaidStateChanged(const RaidInstanceState &state);
-void NotifyRaidReset(const RaidInstanceState &state);
-void NotifyRaidCompleted(const RaidInstanceState &state);
-void NotifyRaidFailed(const RaidInstanceState &state);
+inline RaidModHooksV1 ActiveHooks {};
+inline RaidModCompatibilityStatus Compatibility {};
+
+inline void WriteModuleName(std::string_view moduleName)
+{
+	Compatibility.moduleName.fill('\0');
+	const size_t count = std::min(moduleName.size(), Compatibility.moduleName.size() - 1);
+	std::copy_n(moduleName.begin(), count, Compatibility.moduleName.begin());
+}
+
+inline void Invoke(void (*hook)(const RaidInstanceState &), const RaidInstanceState &state)
+{
+	if (hook != nullptr)
+		hook(state);
+}
+
+} // namespace raid_mod_api_detail
+
+inline void ResetRaidModHooks()
+{
+	raid_mod_api_detail::ActiveHooks = {};
+	raid_mod_api_detail::Compatibility = {};
+}
+
+[[nodiscard]] inline bool RegisterRaidModHooks(std::string_view moduleName, uint32_t apiVersion, const RaidModHooksV1 &hooks)
+{
+	raid_mod_api_detail::Compatibility.requestedVersion = apiVersion;
+	raid_mod_api_detail::Compatibility.activeVersion = CurrentRaidModApiVersion;
+	raid_mod_api_detail::WriteModuleName(moduleName);
+	if (apiVersion != CurrentRaidModApiVersion) {
+		raid_mod_api_detail::Compatibility.compatible = false;
+		return false;
+	}
+	raid_mod_api_detail::Compatibility.compatible = true;
+	raid_mod_api_detail::ActiveHooks = hooks;
+	return true;
+}
+
+[[nodiscard]] inline RaidModCompatibilityStatus GetRaidModCompatibilityStatus()
+{
+	return raid_mod_api_detail::Compatibility;
+}
+
+inline void NotifyRaidStateChanged(const RaidInstanceState &state)
+{
+	raid_mod_api_detail::Invoke(raid_mod_api_detail::ActiveHooks.onRaidStateChanged, state);
+}
+
+inline void NotifyRaidReset(const RaidInstanceState &state)
+{
+	raid_mod_api_detail::Invoke(raid_mod_api_detail::ActiveHooks.onRaidReset, state);
+}
+
+inline void NotifyRaidCompleted(const RaidInstanceState &state)
+{
+	raid_mod_api_detail::Invoke(raid_mod_api_detail::ActiveHooks.onRaidCompleted, state);
+}
+
+inline void NotifyRaidFailed(const RaidInstanceState &state)
+{
+	raid_mod_api_detail::Invoke(raid_mod_api_detail::ActiveHooks.onRaidFailed, state);
+}
 
 } // namespace devilution
